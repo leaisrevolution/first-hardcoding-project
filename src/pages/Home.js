56ea4f576612d162954251a4821148e866386e62
leaisrevolution <leaisrevolution@gmail.com>
2021-11-20 06:28:27 +0900

    버튼이 혼자 커졌다 작아졌다 이슈가 있었는데, 와 영광님 이걸 살리시네..영광님 캐리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,115 +1,70 @@
-import React, { useState, Component } from 'react';
+import React, { useState, Component,useEffect } from 'react';
 import Select from 'react-select';
+import { Link } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 
 const options = [
-    { value: "male", label: "Male" },
-    { value: "female", label: "Female" },
+    { id: "male", value: "male", label: "Male" },
+    { id: "female", value: "female", label: "Female" },
 ];
 
 
 // 이부분 영광님이 수정 도와주심.. 역시 영광님은 천재 갓영광
-// 그의 노고가 물거품이 되지 않도록 다시 코드 이해하고 넘어갑시다.. 또 물어보면 염치도 없지
+// 그의 노고가 물거품이 되지 않도록 다시 코드 이해하고 넘어갑시다..
+// 또 물어보면 염치도 없지
 
 const Home = (props) => {
     const { history } = props;
-    console.log(props)
     const [selectedOption, setSelectedOption] = useState(null);
-    const handleChange = (option) => {
-        setSelectedOption(option);
-        console.log(`Option selected:`, option);
-};
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+    const [gender, setGender] = useState('')
 
-// const customStyles = {
-//     input: (provided, state) => ({
-//     ...provided,
-//     borderBottom: '1px dotted pink',
-//     color: state.isSelected ? 'red' : 'blue',
-//     padding: 20,
-//     }),
-//     control: () => ({
-//       // none of react-select's styles are passed to <Control />
-//     width: 200,
-//     }),
-//     singleValue: (provided, state) => {
-//     const opacity = state.isDisabled ? 0.5 : 1;
-//     const transition = 'opacity 300ms';
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(name + gender);
+    }
 
-//     return { ...provided, opacity, transition };
-//     }
-// }
-
-    // const dispatch = useDispatch();
-    // const [name, setName] =useState('');
-    // const [gender, setGender] = useState('');
-
-    // const handleClickRadioButton = useCallback((radioBtnName) => {
-    //     setGender(radioBtnName)
-    // }, [gender])
-
-    //     const handleClickRadioButton = () => {
-    //     const nonFullKorean = /[ㄱ-ㅎ]/g
-
-    //     if (nonFullKorean.test(name)) {
-    //         setName('')
-    //         setGender('')
-    //         error("이름을 다시 입력해주세요.", {
-    //             position: "top-right"
-    //         })
-    //     } else {
-    //         dispatchEvent(actions.registerUser({name, gender}));
-    //         history.push('/Guide');
-    //     }
-    // }
-
-    //     const handleInputName = (e) => {
-    //     const regex = /[a-z0-9~!@#$%^&*()\-=+_';<>/.`:",[\]?|{}\s*\r\n]/gi
-    //     const fullKorean = /^[ㄱ-ㅎ|가-힣]*$/
-    //     console.log(e.target.value)
-    //     if(regex.test(e.target.value)){
-    //         toast.error("한글만 입력해주세요.",{
-    //             position: "top-right"
-    //         })
-    //         e.target.value = e.target.value.replace(regex, '')
-    //     }
-
-    //     if(fullKorean.test(e.target.value)){
-    //         setName(e.target.value);
-    //     }
-    // }
 
     return (
+        <div>
+            <div className="nav">
+                <div className="nav container"> /* elice */ front-end project</div>
+            </div>
 
-            <div>
-
-                <div className="nav">
-                    <div className="nav container"> /* elice */ front-end project</div>
+            <div className="wrap">
+                <div className="container">
+                    <div className="main">
+                    <h1 className="title"> 직업 가치관 검사 </h1>
+                    <p className="content">
+                        {" "}
+                        당신의 직업 검사 진행을 통해 <br /> 나와 맞는 직업과 직업 가치관, 적합도가 높은 직업을 탐색해봅시다.
+                    </p>
                 </div>
 
-                <div className="wrap">
-                    <div className="container">
-                        <div className="main">
-                        <h1 className="title"> 직업 가치관 검사 </h1>
-                        <p className="content">
-                            {" "}
-                            당신의 직업 검사 진행을 통해 <br /> 나와 맞는 직업과 직업 가치관, 적합도가 높은 직업을 탐색해봅시다.
-                        </p>
-                    </div>
-
                 <div>
-                    <h3 className = "title2">당신에 대해서 알려주세요.</h3>
-                    <hr class="foo" />
 
+                    <h3 className = "title2">당신에 대해서 알려주세요.</h3>
+                    <hr className="foo" />
+
+                </div>
+
+            <form onSubmit={ handleSubmit }>
                 <div className = "Q">
                     <h4 className="qustionHeader"> Q1 </h4>
                     <h5>당신의 이름을 입력해주세요. </h5>
                     <input
-                        name="name"
+                        id="name"
                         className = "inputName"
-                        // onChange={handleInputName}
-                        // value={name}
-                        type="text" />
+                        type="text"
+                        value={name}
+                        onChange={
+                            (event) => {
+                                setName(event.target.value);
+                            }
+                        }
+                    />
                 </div>
 
                 <div className = "Q">
@@ -118,9 +73,14 @@ const Home = (props) => {
                     <input
                         name ="age"
                         className = "inputName"
-                        // onChange={handleInputAge}
-                        // value={age}
-                        type="text" />
+                        type="text"
+                        value={age}
+                        onChange={
+                            (event) => {
+                                setAge(event.target.value);
+                            }// 이거 처럼 setGender도 해주셔야해요.
+                        }
+                    />
                 </div>
 
                 <div className = "Q">
@@ -128,36 +88,46 @@ const Home = (props) => {
                     <h5>당신의 성별을 선택해주세요. </h5>
                 </div>
 
-                {/* <select className = "gender">
-                    <option className ="genderSelet">선택</option>
-                    <option value="남자">남자</option>
-                    <option value="여자">여자</option>
-                </select> */}
 
-                        <Select
-                            value={selectedOption}
-                            onChange={handleChange}
-                            options={options}
-                        />
+                <Select
+                    value={selectedOption}
+                    onChange={(option)=>{
+                        setSelectedOption(option)
+                        setGender(option.value)
+                        console.log(gender)
+                    }}
+                    options={options}
+                />
 
-                <div className="startbtn">
+
+                <div className="startbtn">{
+                    (name.length && gender.length && age.length) ?
+                    <Link to="/guide">
+                        <button
+                            type="submit"
+                            className="start"
+                            onClick={ (event) => {
+                                    history.push("/guide");
+                                }}>
+                        다음으로
+                        </button>
+                    </Link>
+                    ://안녕하세요
                     <button
+                        type = "submit"
                         className="start"
-                        // disabled={!(inputname && gender)}
-                        onClick={() => {
-                            history.push("/guide");
-                            }}>
-                            다음으로
+                        disabled>
+                        다음으로
                     </button>
+                    }
                 </div>
 
-                </div>
+            </form>
 
-                    </div>
+
                 </div>
             </div>
-
-
+        </div>
     );
 };
 
