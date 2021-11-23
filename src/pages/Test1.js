5c9b3cfda714a720e399b9fbb7de29309aa68d3e
leaisrevolution <leaisrevolution@gmail.com>
2021-11-23 16:23:49 +0900

    하드코딩으로 하기로 결정!!! 나중에 더 잘하면 되지!!!

--- src/pages/Test1.js
+++ src/pages/Test1.js
@@ -3,7 +3,8 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import MainNav from '../components/mainNav';
 
-export default function Test1(){
+export default function Test1(props){
+    const { history } = props;
     const [answer, setAnswer] = useState({});
     const [check,setCheck] = useState({
         ans_1 : "",
@@ -24,14 +25,24 @@ export default function Test1(){
                 question: res.data.RESULT[0].question,
                 answer01: res.data.RESULT[0].answer01,
                 answer02: res.data.RESULT[0].answer02,
+                answer03: res.data.RESULT[0].answer03,
+                answer04: res.data.RESULT[0].answer04,
                 answer11: res.data.RESULT[1].answer01,
                 answer12: res.data.RESULT[1].answer02,
+                answer13: res.data.RESULT[1].answer03,
+                answer14: res.data.RESULT[1].answer04,
                 answer21: res.data.RESULT[2].answer01,
                 answer22: res.data.RESULT[2].answer02,
+                answer23: res.data.RESULT[2].answer03,
+                answer24: res.data.RESULT[2].answer04,
                 answer31: res.data.RESULT[3].answer01,
                 answer32: res.data.RESULT[3].answer02,
+                answer33: res.data.RESULT[3].answer03,
+                answer34: res.data.RESULT[3].answer04,
                 answer41: res.data.RESULT[4].answer01,
                 answer42: res.data.RESULT[4].answer02,
+                answer43: res.data.RESULT[4].answer03,
+                answer44: res.data.RESULT[4].answer04,
             })
     }
     useEffect(()=>{
@@ -60,7 +71,7 @@ export default function Test1(){
                         <div className="questionHead">
 
                             <h4 className="qustionNumber">
-                                Q1
+                                Q1.
                             </h4>
 
                             <h5 className="qustionText">{answer.question}</h5>
@@ -73,22 +84,23 @@ export default function Test1(){
                                     <div>
                                         <button className= "answerBtn"
                                             type="radio"
-                                            name="answer"
+                                            name="ans_1"
                                             value={answer.answer01}
+                                            onClick={handleChange}
                                             onChange={
                                                 (event) => {
                                                     setAnswer(event.target.value);
                                                 }
                                             }
-                                            // onClick={}
                                         >{answer.answer01}</button>
                                     </div>
 
                                     <div>
                                     <button className="answerBtn2"
                                         type="radio"
-                                        name="answer"
+                                        name="ans_1"
                                         value={answer.answer02}
+                                        onClick={handleChange}
                                         onChange={
                                             (event) => {
                                                 setAnswer(event.target.value);
@@ -97,20 +109,28 @@ export default function Test1(){
                                     > {answer.answer02} </button>
 
                                     </div>
-
-                                <hr className="foo2" />
                             </div>
+
+                            <div1>
+                                <p className="explanation">
+                                > {answer.answer01} : {answer.answer03}
+                                <br />
+                                > {answer.answer02} : {answer.answer04}  </p>
+                            </div1>
+
                         </div>
 
 
                     </div1>
+
+                    <hr className="foo3" />
 
                     <div2>
 
                         <div className="questionHead">
 
                             <h4 className="qustionNumber">
-                                Q2
+                                Q2.
                             </h4>
 
                             <h5 className="qustionText">{answer.question}</h5>
@@ -123,8 +143,9 @@ export default function Test1(){
                                     <div>
                                         <button className= "answerBtn"
                                             type="radio"
-                                            name="answer"
+                                            name="ans_2"
                                             value={answer.answer11}
+                                            onClick={handleChange}
                                             onChange={
                                                 (event) => {
                                                     setAnswer(event.target.value);
@@ -137,8 +158,9 @@ export default function Test1(){
                                     <div>
                                     <button className="answerBtn2"
                                         type="radio"
-                                        name="answer"
+                                        name="ans_2"
                                         value={answer.answer12}
+                                        onClick={handleChange}
                                         onChange={
                                             (event) => {
                                                 setAnswer(event.target.value);
@@ -148,19 +170,27 @@ export default function Test1(){
 
                                     </div>
 
-                                <hr className="foo2" />
                             </div>
+
+                            <div2>
+                                    <p className="explanation">
+                                    > {answer.answer11} : {answer.answer13}
+                                    <br />
+                                    > {answer.answer12} : {answer.answer14}  </p>
+                            </div2>
+
                         </div>
 
-
                     </div2>
+
+                    <hr className="foo3" />
 
                     <div3>
 
                         <div className="questionHead">
 
                             <h4 className="qustionNumber">
-                                Q1
+                                Q3.
                             </h4>
 
                             <h5 className="qustionText">{answer.question}</h5>
@@ -173,8 +203,9 @@ export default function Test1(){
                                     <div>
                                         <button className= "answerBtn"
                                             type="radio"
-                                            name="answer"
+                                            name="ans_3"
                                             value={answer.answer21}
+                                            onClick={handleChange}
                                             onChange={
                                                 (event) => {
                                                     setAnswer(event.target.value);
@@ -187,8 +218,9 @@ export default function Test1(){
                                     <div>
                                     <button className="answerBtn2"
                                         type="radio"
-                                        name="answer"
+                                        name="ans_3"
                                         value={answer.answer22}
+                                        onClick={handleChange}
                                         onChange={
                                             (event) => {
                                                 setAnswer(event.target.value);
@@ -200,17 +232,27 @@ export default function Test1(){
 
                                 <hr className="foo2" />
                             </div>
+
+                            <div3>
+                                    <p className="explanation">
+                                    > {answer.answer21} : {answer.answer23}
+                                    <br />
+                                    > {answer.answer22} : {answer.answer24}  </p>
+                            </div3>
+
                         </div>
 
 
                     </div3>
+
+                    <hr className="foo3" />
 
                     <div4>
 
                         <div className="questionHead">
 
                             <h4 className="qustionNumber">
-                                Q1
+                                Q4.
                             </h4>
 
                             <h5 className="qustionText">{answer.question}</h5>
@@ -223,44 +265,56 @@ export default function Test1(){
                                     <div>
                                         <button className= "answerBtn"
                                             type="radio"
-                                            name="answer"
-                                            value={answer.answer01}
+                                            name="ans_4"
+                                            value={answer.answer31}
+                                            onClick={handleChange}
                                             onChange={
                                                 (event) => {
                                                     setAnswer(event.target.value);
                                                 }
                                             }
                                             // onClick={}
-                                        >{answer.answer01}</button>
+                                        >{answer.answer31}</button>
                                     </div>
 
                                     <div>
                                     <button className="answerBtn2"
                                         type="radio"
-                                        name="answer"
-                                        value={answer.answer02}
+                                        name="ans_4"
+                                        value={answer.answer32}
+                                        onClick={handleChange}
                                         onChange={
                                             (event) => {
                                                 setAnswer(event.target.value);
                                             }
                                         }
-                                    > {answer.answer02} </button>
+                                    > {answer.answer32} </button>
 
                                     </div>
 
                                 <hr className="foo2" />
                             </div>
+
+                            <div3>
+                                    <p className="explanation">
+                                    > {answer.answer31} : {answer.answer33}
+                                    <br />
+                                    > {answer.answer32} : {answer.answer34}  </p>
+                            </div3>
+
                         </div>
 
 
                     </div4>
+
+                    <hr className="foo3" />
 
                     <div5>
 
                         <div className="questionHead">
 
                             <h4 className="qustionNumber">
-                                Q1
+                                Q5.
                             </h4>
 
                             <h5 className="qustionText">{answer.question}</h5>
@@ -273,65 +327,99 @@ export default function Test1(){
                                     <div>
                                         <button className= "answerBtn"
                                             type="radio"
-                                            name="answer"
-                                            value={answer.answer01}
+                                            name="ans_5"
+                                            value={answer.answer41}
+                                            onClick={handleChange}
                                             onChange={
                                                 (event) => {
                                                     setAnswer(event.target.value);
                                                 }
                                             }
                                             // onClick={}
-                                        >{answer.answer01}</button>
+                                        >{answer.answer41}</button>
                                     </div>
 
                                     <div>
                                     <button className="answerBtn2"
                                         type="radio"
-                                        name="answer"
-                                        value={answer.answer02}
+                                        name="ans_5"
+                                        value={answer.answer42}
+                                        onClick={handleChange}
                                         onChange={
                                             (event) => {
                                                 setAnswer(event.target.value);
                                             }
                                         }
-                                    > {answer.answer02} </button>
+                                    > {answer.answer42} </button>
 
                                     </div>
-
                                 <hr className="foo2" />
+
                             </div>
+
+                            <div3>
+                                    <p className="explanation">
+                                    > {answer.answer41} : {answer.answer43}
+                                    <br />
+                                    > {answer.answer42} : {answer.answer44}  </p>
+                            </div3>
+
                         </div>
-
-
                     </div5>
+
+                    <hr className="foo3" />
+
+                    {/* <Link to='/guide'><button type="submit">이전</button></Link>
+                    <button type="submit" onClick={handleSubmit}>다음</button> */}
+
+
+                <div Id="btn">
+
+                    <button
+                        type="submit"
+                        className="back"
+                        onClick={() => {history.push("/guide");}}> 뒤로가기</button>
+
+                    <button
+
+                        type="submit"
+                        className="start"
+                        onClick={() => {
+                            history.push("/test2")
+                            // if (valid)
+                            // history.push("/test1");
+                            // else
+                            // setError("예시 항목을 선택해주세요.")
+                        }}
+                        > 다음으로
+
+                    </button>
+
+                    {/* { (answer01 === '1') || (answer02 === '2') ?
+                        <Link to="/test1">
+                            <button
+                                type="submit"
+                                className="start"
+                                onClick={() =>
+                                {history.push("/test1");}}> 검사시작
+                            </button>
+                        </Link>
+                        :
+                            <button
+                                type="submit"
+                                className="start"
+                                disabled ="false">
+                                검사시작
+                            </button>
+                    } */}
+
+                </div>
 
                 </div>
 
 
 
-            <p>Q1.{answer.question}</p>
-            <input type="radio" name="ans_1" value={answer.answer01} onClick={handleChange}/> {answer.answer01}
-            <input type="radio" name="ans_1" value={answer.answer02} onClick={handleChange}/> {answer.answer02}
-            <br/>
-            <p>Q2.{answer.question}</p>
-            <input type="radio" name="ans_2" value={answer.answer11} onClick={handleChange}/> {answer.answer11}
-            <input type="radio" name="ans_2" value={answer.answer12} onClick={handleChange}/> {answer.answer12}
-            <br/>
-            <p>Q3.{answer.question}</p>
-            <input type="radio" name="ans_3" value={answer.answer21} onClick={handleChange}/> {answer.answer21}
-            <input type="radio" name="ans_3" value={answer.answer21} onClick={handleChange}/> {answer.answer22}
-            <br/>
-            <p>Q4.{answer.question}</p>
-            <input type="radio" name="ans_4" value={answer.answer31} onClick={handleChange}/> {answer.answer31}
-            <input type="radio" name="ans_4" value={answer.answer32} onClick={handleChange}/> {answer.answer32}
-            <br/>
-            <p>Q5.{answer.question}</p>
-            <input type="radio" name="ans_5" value={answer.answer41} onClick={handleChange}/> {answer.answer41}
-            <input type="radio" name="ans_5" value={answer.answer42} onClick={handleChange}/> {answer.answer42}
-            <br/>
 
-            <Link to='/guide'><button type="submit">이전</button></Link>
-            <button type="submit" onClick={handleSubmit}>다음</button>
         </div>
 
     )
