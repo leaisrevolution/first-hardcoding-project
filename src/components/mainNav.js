5def3c4ed58246f2af0912ca908caac453808ef4
leaisrevolution <leaisrevolution@gmail.com>
2021-11-28 11:30:58 +0900

    none

--- src/components/mainNav.js
+++ src/components/mainNav.js
@@ -1,12 +1,17 @@
 
 import React from "react";
+import Logo from "../image/elice_logo.png";
 
 
 
-function mainNav() {
+function mainNav(props) {
+
     return (
         <div className="nav">
-            <div className="nav container"> /* elice */ front-end project</div>
+            <div className="nav container">
+                elice front end project
+            </div>
+
         </div>
     );
 }
